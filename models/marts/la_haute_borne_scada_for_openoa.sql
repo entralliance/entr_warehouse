94640294503ac96d4baa4b22c975bf211be81824
Charles Henderson <charlie@cehenderson.com>
2021-07-29 11:28:56 +0200

    Enabling projection of data to OpenOA

--- models/marts/la_haute_borne_scada_for_openoa.sql
+++ models/marts/la_haute_borne_scada_for_openoa.sql
@@ -8,14 +8,15 @@ WITH keyedSource as (
 --     FROM `entrhub.entr_warehouse.entr_data_standardized`
 )
 SELECT
-    MAX (wind_turbine_name) as id,
-    CAST(MAX(date_time) as Timestamp) as time,
-    CAST(MAX(IF(entr_tag_name = "WMET.HorWdSpd", tag_value, NULL)) as Numeric) AS wmet_wdspd_avg,
-    CAST(MAX(IF(entr_tag_name = "WMET.HorWdDir", tag_value, NULL)) as Numeric) AS wmet_HorWdDir_avg,
-    CAST(MAX(IF(entr_tag_name = "WMET.HorWdDirRel", tag_value, NULL)) as Numeric) AS wmet_VaneDir_avg,
-    CAST(MAX(IF(entr_tag_name = "WNAC.Dir", tag_value, NULL)) as Numeric) AS wyaw_YwAng_avg,
-    CAST(MAX(IF(entr_tag_name = "WTOW.GndCtlTmp", tag_value, NULL)) as Numeric) AS wmet_EnvTmp_avg,
-    CAST(MAX(IF(entr_tag_name = "WROT.BlPthAngVal", tag_value, NULL)) as Numeric) AS wrot_BlPthAngVal1_avg,
+    MAX (wind_turbine_name) as Wind_turbine_name,
+    CAST(MAX(date_time) as Timestamp) as Date_time,
+    CAST(MAX(IF(entr_tag_name = "WTUR.W", tag_value, NULL)) as Numeric) AS P_avg, -- wtur_W_avg
+    CAST(MAX(IF(entr_tag_name = "WMET.HorWdSpd", tag_value, NULL)) as Numeric) AS Ws_avg, -- wmet_wdspd_avg
+    CAST(MAX(IF(entr_tag_name = "WMET.HorWdDir", tag_value, NULL)) as Numeric) AS Wa_avg, -- wmet_HorWdDir_avg
+    CAST(MAX(IF(entr_tag_name = "WMET.HorWdDirRel", tag_value, NULL)) as Numeric) AS Va_avg, -- wmet_VaneDir_avg
+    CAST(MAX(IF(entr_tag_name = "WNAC.Dir", tag_value, NULL)) as Numeric) AS Ya_avg, -- wyaw_YwAng_avg
+    CAST(MAX(IF(entr_tag_name = "WTOW.GndCtlTmp", tag_value, NULL)) as Numeric) AS Ot_avg, -- wmet_EnvTmp_avg
+    CAST(MAX(IF(entr_tag_name = "WROT.BlPthAngVal", tag_value, NULL)) as Numeric) AS Ba_avg -- wrot_BlPthAngVal1_avg
 FROM keyedSource
 GROUP BY key
 ORDER BY key
