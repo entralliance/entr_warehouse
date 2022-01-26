65c41b5c077861bc27c8291db960060e86f00e46
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-01-26 04:57:49 -0500

    update profile copier and add backup file pattern to .gitignore

--- scripts/update_dbt_profiles.sh
+++ scripts/update_dbt_profiles.sh
@@ -3,9 +3,8 @@
 # make .dbt directory if it doesn't already exist
 mkdir ~/.dbt -p
 
-# copy profiles.yml to ~/.dbt
-cp ../profiles.yml ~/.dbt/profiles.yml -i
+# copy profiles.yml to ~/.dbt - DEPENDS ON entr_warehouse MOUNT LOCATION
+cp ~/host/entr_warehouse/profiles.yml ~/.dbt/profiles.yml -i
 
 # force Linux line endings in the file
-cd ~/.dbt
-sed 's/\r$//' ~/.dbt/profiles.yml > ~/.dbt/profiles.yml
+sed -i.bak 's/\r$//' ~/.dbt/profiles.yml
