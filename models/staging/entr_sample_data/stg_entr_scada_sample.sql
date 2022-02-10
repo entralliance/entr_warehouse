1ce844e1dfa12cbcbb040b07b8b09e4111645e67
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-10 08:55:52 -0500

    point SCADA staging model to new names

--- models/staging/entr_sample_data/stg_entr_scada_sample.sql
+++ models/staging/entr_sample_data/stg_entr_scada_sample.sql
@@ -1,22 +1,29 @@
+{# 
+    TODO:
+        - tie in turbine dimension
+#}
+
+{% set src_model = 'int_entr_scada_sample' %}
+
 with
-     la_haute as (select * from {{ref('stg_la_haute_borne_data_sample')}}),
-     map as (select * from {{ref('stg_la_haute_tag_mapping')}}),
-     std_tags as (select * from {{ref('stg_entr_tag_list')}}),
+    src as (select * from {{ref(src_model)}}),
+    map as (select * from {{ref('map_entr_scada_sample')}}),
+    std_tags as (select * from {{ref('dim_entr_tag_list')}}),
 
-la_haute_molten as (
-    {{dbt_utils.unpivot(ref('stg_la_haute_borne_data_sample'),
-    cast_to='numeric',
-    exclude=['wind_turbine_name','date_time'],
-    field_name='scada_tag_name',
-    value_name='tag_value')}}
-),
-
-tag_join as (
-    select
-        map.tag_name as entr_tag_name,
-        la_haute_molten.*
-    from la_haute_molten
-    left join map on split(lower(la_haute_molten.scada_tag_name),"_")[0] = lower(map.variable_name)
+src_molten as (
+    {{
+        dbt_utils.unpivot(
+            ref(src_model),
+            cast_to='numeric',
+            exclude=['wind_turbine_name','date_time'],
+            field_name='scada_tag_name',
+            value_name='tag_value'
+        )
+    }}
 )
 
-select * from tag_join
+select
+    map.tag_name as entr_tag_name,
+    src_molten.*
+from src_molten
+left join map on split(lower(src_molten.scada_tag_name),"_")[0] = lower(map.variable_name)
