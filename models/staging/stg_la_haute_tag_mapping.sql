ab98f8e6db599309da57782e05312c1982338ad1
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-10 02:31:00 -0500

    make staging table cross-db compatible

--- models/staging/stg_la_haute_tag_mapping.sql
+++ models/staging/stg_la_haute_tag_mapping.sql
@@ -1,10 +1,15 @@
-select
-    CAST(tag_name as string) as tag_name,
-    CAST(tag_is_new as string) as tag_is_new,
-    CAST(variable_name as string) as variable_name,
-    CAST(variable_long_name as string) as variable_long_name,
-    CAST(unit_long_name as string) as unit_long_name,
-    CAST(comment as string) as comment
+{{
+    config(materialized='table')
+}}
 
-from
-    {{ref('la_haute_tag_mapping')}}
+with
+    src as (select * from {{ref('la_haute_tag_mapping')}})
+
+select
+    CAST(tag_name as {{dbt_utils.type_string()}}) as tag_name,
+    CAST(tag_is_new as {{dbt_utils.type_string()}}) as tag_is_new,
+    CAST(variable_name as {{dbt_utils.type_string()}}) as variable_name,
+    CAST(variable_long_name as {{dbt_utils.type_string()}}) as variable_long_name,
+    CAST(unit_long_name as {{dbt_utils.type_string()}}) as unit_long_name,
+    CAST(comment as {{dbt_utils.type_string()}}) as comment
+from src
