c96c33bfdd75e9b3013bbd8cd6b473b11d96cb96
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-14 15:00:58 -0500

    clean up styling

--- models/staging/entr_sample_data/intermediate/int_entr_scada_sample__cast.sql
+++ models/staging/entr_sample_data/intermediate/int_entr_scada_sample__cast.sql
@@ -1,5 +1,8 @@
+with
+    src as (select * from {{ ref('seed_la_haute_borne_data_sample') }})
+
 select
-    CAST(Wind_turbine_name as {{dbt_utils.type_string()}}) as wind_turbine_name,
+    cast(Wind_turbine_name as {{dbt_utils.type_string()}}) as wind_turbine_name,
     -- {# Date_time,
     -- to_utc_timestamp(
     --     to_timestamp(Date_time, "yyyy-MM-dd'T'HH:mm:ssXXX"),
@@ -7,13 +10,11 @@ select
     --     ) as conv_date_time,
     -- CAST(Date_time as {{dbt_utils.type_timestamp()}}) as raw_datetime_cast, #}
     {{dbt_date.convert_timezone('CAST(Date_time as ' ~ dbt_utils.type_timestamp() ~ ')', 'UTC')}} as date_time,
-    CAST(Ba_avg as {{dbt_utils.type_numeric()}}) as ba_avg,
-    CAST(P_avg  as {{dbt_utils.type_numeric()}}) as p_avg,
-    CAST(Ws_avg as {{dbt_utils.type_numeric()}}) as ws_avg,
-    CAST(Va_avg as {{dbt_utils.type_numeric()}}) as va_avg,
-    CAST(Ot_avg as {{dbt_utils.type_numeric()}}) as ot_avg,
-    CAST(Ya_avg as {{dbt_utils.type_numeric()}}) as ya_avg,
-    CAST(Wa_avg as {{dbt_utils.type_numeric()}}) as wa_avg
-
-from
-    {{ref('seed_la_haute_borne_data_sample')}}
+    cast(Ba_avg as {{dbt_utils.type_numeric()}}) as ba_avg,
+    cast(P_avg  as {{dbt_utils.type_numeric()}}) as p_avg,
+    cast(Ws_avg as {{dbt_utils.type_numeric()}}) as ws_avg,
+    cast(Va_avg as {{dbt_utils.type_numeric()}}) as va_avg,
+    cast(Ot_avg as {{dbt_utils.type_numeric()}}) as ot_avg,
+    cast(Ya_avg as {{dbt_utils.type_numeric()}}) as ya_avg,
+    cast(Wa_avg as {{dbt_utils.type_numeric()}}) as wa_avg
+from src
