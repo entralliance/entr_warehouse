3478e78ca618f4b59141525a355ae01d9e1bfa6b
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-17 12:23:52 -0500

    create first staging model for the plant data

---
+++ models/staging/entr_sample_data/intermediate/int_entr_plant_sample__cast.sql
@@ -0,0 +1,15 @@
+with
+    src as (select * from {{ ref('seed_plant_data_sample') }})
+
+select
+    -- {# Date_time,
+    -- to_utc_timestamp(
+    --     to_timestamp(Date_time, "yyyy-MM-dd'T'HH:mm:ssXXX"),
+    --     current_timezone()
+    --     ) as conv_date_time,
+    -- CAST(Date_time as {{dbt_utils.type_timestamp()}}) as raw_datetime_cast, #}
+    {{dbt_date.convert_timezone('CAST(time_utc as ' ~ dbt_utils.type_timestamp() ~ ')', 'UTC')}} as time_utc,
+    cast(net_energy_kwh as {{dbt_utils.type_numeric()}}) as net_energy_kwh,
+    cast(availability_kwh as {{dbt_utils.type_numeric()}}) as availability_kwh,
+    cast(curtailment_kwh as {{dbt_utils.type_numeric()}}) as curtailment_kwh
+from src
