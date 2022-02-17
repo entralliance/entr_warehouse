74166b3e8d389bd424c393193721f4779589ada7
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-17 13:49:17 -0500

    minor clean up

--- models/staging/entr_sample_data/intermediate/int_entr_plant_sample__cast.sql
+++ models/staging/entr_sample_data/intermediate/int_entr_plant_sample__cast.sql
@@ -9,7 +9,7 @@ select
     --     current_timezone()
     --     ) as conv_date_time,
     -- CAST(Date_time as {{dbt_utils.type_timestamp()}}) as raw_datetime_cast, #}
-    {{dbt_date.convert_timezone('CAST(time_utc as ' ~ dbt_utils.type_timestamp() ~ ')', 'UTC')}} as date_time,
+    {{dbt_date.convert_timezone('cast(time_utc as ' ~ dbt_utils.type_timestamp() ~ ')', 'UTC')}} as date_time,
     cast(net_energy_kwh as {{dbt_utils.type_numeric()}}) as net_energy_kwh,
     cast(availability_kwh as {{dbt_utils.type_numeric()}}) as availability_kwh,
     cast(curtailment_kwh as {{dbt_utils.type_numeric()}}) as curtailment_kwh
--- models/staging/entr_sample_data/intermediate/int_entr_scada_sample__cast.sql
+++ models/staging/entr_sample_data/intermediate/int_entr_scada_sample__cast.sql
@@ -2,7 +2,7 @@ with
     src as (select * from {{ ref('seed_la_haute_borne_data_sample') }})
 
 select
-    cast(Wind_turbine_name as {{dbt_utils.type_string()}}) as wind_turbine_name,
+    cast(wind_turbine_name as {{dbt_utils.type_string()}}) as wind_turbine_name,
     -- {# Date_time,
     -- to_utc_timestamp(
     --     to_timestamp(Date_time, "yyyy-MM-dd'T'HH:mm:ssXXX"),
@@ -10,11 +10,11 @@ select
     --     ) as conv_date_time,
     -- CAST(Date_time as {{dbt_utils.type_timestamp()}}) as raw_datetime_cast, #}
     {{dbt_date.convert_timezone('CAST(Date_time as ' ~ dbt_utils.type_timestamp() ~ ')', 'UTC')}} as date_time,
-    cast(Ba_avg as {{dbt_utils.type_numeric()}}) as ba_avg,
-    cast(P_avg  as {{dbt_utils.type_numeric()}}) as p_avg,
-    cast(Ws_avg as {{dbt_utils.type_numeric()}}) as ws_avg,
-    cast(Va_avg as {{dbt_utils.type_numeric()}}) as va_avg,
-    cast(Ot_avg as {{dbt_utils.type_numeric()}}) as ot_avg,
-    cast(Ya_avg as {{dbt_utils.type_numeric()}}) as ya_avg,
-    cast(Wa_avg as {{dbt_utils.type_numeric()}}) as wa_avg
+    cast(ba_avg as {{dbt_utils.type_numeric()}}) as ba_avg,
+    cast(p_avg  as {{dbt_utils.type_numeric()}}) as p_avg,
+    cast(ws_avg as {{dbt_utils.type_numeric()}}) as ws_avg,
+    cast(va_avg as {{dbt_utils.type_numeric()}}) as va_avg,
+    cast(ot_avg as {{dbt_utils.type_numeric()}}) as ot_avg,
+    cast(ya_avg as {{dbt_utils.type_numeric()}}) as ya_avg,
+    cast(wa_avg as {{dbt_utils.type_numeric()}}) as wa_avg
 from src
