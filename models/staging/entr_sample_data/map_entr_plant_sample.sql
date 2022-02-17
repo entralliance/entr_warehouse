23f58ee3c55215602ba74ccc15597efef17241d8
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-02-17 13:17:04 -0500

    create staging model for plant data sample

---
+++ models/staging/entr_sample_data/map_entr_plant_sample.sql
@@ -0,0 +1,11 @@
+{{
+    config(materialized='table')
+}}
+
+with
+    src as (select * from {{ref('seed_plant_data_tag_map')}})
+
+select
+    cast(tag_name as {{dbt_utils.type_string()}}) as tag_name,
+    cast(variable_name as {{dbt_utils.type_string()}}) as variable_name
+from src
---
+++ models/staging/entr_sample_data/stg_entr_plant_sample.sql
@@ -0,0 +1,29 @@
+{# 
+    TODO:
+        - tie in plant dimension
+#}
+
+{% set src_model = 'int_entr_plant_sample__cast' %}
+
+with
+    src as (select * from {{ref(src_model)}}),
+    map as (select * from {{ref('map_entr_plant_sample')}}),
+    std_tags as (select * from {{ref('dim_entr_tag_list')}}),
+
+src_molten as (
+    {{
+        dbt_utils.unpivot(
+            ref(src_model),
+            cast_to='numeric',
+            exclude=['plant_name','date_time'],
+            field_name='scada_tag_name',
+            value_name='tag_value'
+        )
+    }}
+)
+
+select
+    map.tag_name as entr_tag_name,
+    src_molten.*
+from src_molten
+left join map on split(lower(src_molten.scada_tag_name),"_")[0] = lower(map.variable_name)
