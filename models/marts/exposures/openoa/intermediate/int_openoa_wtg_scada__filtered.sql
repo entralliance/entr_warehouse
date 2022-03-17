c09b12d135e8dadf4700f2572f8482effb3a70ef
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-03-17 11:56:54 -0400

    add derived energy tag

--- models/marts/exposures/openoa/intermediate/int_openoa_wtg_scada__filtered.sql
+++ models/marts/exposures/openoa/intermediate/int_openoa_wtg_scada__filtered.sql
@@ -3,21 +3,39 @@
 with
     src as (select * from {{ ref(src_model) }}),
     wtg_dim as (select * from {{ ref('dim_asset_wind_turbine') }}),
-    tag_dim as (select * from {{ ref('dim_entr_tag_list') }})
+    tag_dim as (select * from {{ ref('dim_entr_tag_list') }}),
+
+energy_union as (
+    select * from src
+    where entr_tag_id in (
+        1349,
+        2456,
+        1104,
+        1089,
+        1058,
+        1162,
+        1074
+    )
+
+    union all
+
+    select
+        wind_turbine_id,
+        2378 as entr_tag_id, -- WTUR.SupWh
+        date_time,
+        tag_value / 6 as tag_value,
+        interval_n,
+        interval_units,
+        'derived' as value_type,
+        {{dbt_utils.concat(['value_units', "'h'"])}} value_units,
+        {{dbt_utils.concat(['standard_units', "'h'"])}} standard_units
+    from (select * from src where entr_tag_id = 2456)
+)
 
 select
     wtg_dim.wind_turbine_name,
     tag_dim.entr_tag_name,
-    src.*
-from src
+    energy_union.*
+from energy_union
 left join wtg_dim using(wind_turbine_id)
 left join tag_dim using(entr_tag_id)
-where entr_tag_id in (
-    1349,
-    2456,
-    1104,
-    1089,
-    1058,
-    1162,
-    1074
-)
