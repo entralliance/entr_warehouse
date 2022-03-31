2560aa6fb678d1f10183ca5c142bdcac885e08a6
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-03-31 10:33:08 -0400

    add more dim cols and make final col selection explicit

--- models/marts/exposures/openoa/intermediate/int_openoa_wtg_scada__filtered.sql
+++ models/marts/exposures/openoa/intermediate/int_openoa_wtg_scada__filtered.sql
@@ -1,7 +1,5 @@
-{% set src_model = 'fct_entr_wtg_scada' %}
-
 with
-    src as (select * from {{ ref(src_model) }}),
+    src as (select * from {{ ref('fct_entr_wtg_scada') }}),
     wtg_dim as (select * from {{ ref('dim_asset_wind_turbine') }}),
     tag_dim as (select * from {{ ref('dim_entr_tag_list') }}),
 
@@ -33,9 +31,18 @@ energy_union as (
 )
 
 select
+    wtd_dim.plant_id,
+    plant_dim.plant_name,
+    wind_turbine_id,
     wtg_dim.wind_turbine_name,
+    entr_tag_id,
     tag_dim.entr_tag_name,
-    energy_union.*
+    energy_union.tag_value,
+    energy_union.interval_n,
+    energy_union.interval_units,
+    energy_union.value_type,
+    energy_union.value_units,
+    energy_union.standard_units
 from energy_union
 left join wtg_dim using(wind_turbine_id)
 left join tag_dim using(entr_tag_id)
