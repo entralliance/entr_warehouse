c5bb7ef63be5b2b36d422fca48383c4ba8e2dedd
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-04-05 03:08:19 -0400

    create basic staging models for reanalysis data

---
+++ models/staging/entr_sample_data/stg_merra2_sample.sql
@@ -0,0 +1,13 @@
+select
+    1 as plant_id,
+    cast( datetime as {{dbt_utils.type_timestamp()}} ) as date_time,
+    cast( u_ms as {{dbt_utils.type_numeric()}} ) as u_ms,
+    cast( v_ms as {{dbt_utils.type_numeric()}} ) as v_ms,
+    cast( temperature_K as {{dbt_utils.type_numeric()}} ) as temperature_K,
+    cast( surf_pres_Pa as {{dbt_utils.type_numeric()}} ) as surf_pres_Pa,
+    cast( windspeed_ms as {{dbt_utils.type_numeric()}} ) as windspeed_ms,
+    cast( winddirection_deg as {{dbt_utils.type_numeric()}} ) as winddirection_deg,
+    cast( rho_kgm3 as {{dbt_utils.type_numeric()}} ) as rho_kgm3,
+    1 as interval_n,
+    'hour' as interval_units
+from {{ref('seed_la_haute_borne_merra2_sample')}}
