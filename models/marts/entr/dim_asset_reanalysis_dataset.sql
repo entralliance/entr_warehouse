db6e4d54f75d68766c02b4ee9b83a876272d19f1
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-05-02 17:09:11 -0400

    create reanalysis dataset dimension

---
+++ models/marts/entr/dim_asset_reanalysis_dataset.sql
@@ -0,0 +1,8 @@
+{{
+    config(materialized='table')
+}}
+
+select
+    cast(reanalysis_dataset_id as {{dbt_utils.type_int()}}) as reanalysis_dataset_id,
+    cast(reanalysis_dataset_name as {{dbt_utils.type_string()}} as reanalysis_dataset_name
+from {{ref('seed_asset_reanalysis_dataset')}}
