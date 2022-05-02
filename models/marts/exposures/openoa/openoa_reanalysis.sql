82ab2be3f59475f961a0cf15a4c92c169e673dd0
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-05-02 17:31:45 -0400

    create reanalysis OpenOA exposure

--- models/marts/exposures/openoa/openoa_reanalysis.sql
+++ models/marts/exposures/openoa/openoa_reanalysis.sql
@@ -1,24 +1,22 @@
+{% set src_model = 'int_openoa_reanalysis_data__filtered' %}
+
 with
-    era5 as (select * from {{ref('stg_era5_sample')}}),
-    merra2 as (select * from {{ref('stg_merra2_sample')}}),
-    plant_dim as (select * from {{ref('dim_asset_wind_plant')}}),
-
-source_union as (
-    select
-        'ERA5' as reanalysis_data_set,
-        *
-    from era5
-
-    union all
-
-    select
-        'MERRA-2' as reanalysis_data_set,
-        *
-    from merra2
-)
+    src as (select * from {{ ref(src_model) }})
 
 select
-    plant_dim.plant_name,
-    source_union.*
-from source_union
-left join plant_dim using (plant_id)
+    plant_id,
+    plant_name,
+    reanalysis_dataset_id,
+    reanalysis_dataset_name,
+    date_time,
+    {{  
+        dbt_utils.pivot(
+            column = 'entr_tag_name',
+            values = dbt_utils.get_column_values(table=ref(src_model), column='entr_tag_name'),
+            then_value = 'tag_value',
+            else_value = 'null',
+            agg = 'avg',
+            quote_identifiers = true) 
+    }}
+from src
+{{ dbt_utils.group_by(n=5) }}
