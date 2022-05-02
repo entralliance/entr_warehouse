51aa67aea6beda655cf435c28001dfa39a658588
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-05-02 17:12:32 -0400

    create staging models for reanalysis data

--- models/staging/entr_sample_data/intermediate/int_entr_merra2_sample__cast.sql
+++ models/staging/entr_sample_data/intermediate/int_entr_merra2_sample__cast.sql
@@ -1,5 +1,6 @@
 select
     1 as plant_id,
+    1 as reanalysis_dataset_id,
     cast( datetime as {{dbt_utils.type_timestamp()}} ) as date_time,
     cast( u_ms as {{dbt_utils.type_numeric()}} ) as u_ms,
     cast( v_ms as {{dbt_utils.type_numeric()}} ) as v_ms,
@@ -7,7 +8,5 @@ select
     cast( surf_pres_Pa as {{dbt_utils.type_numeric()}} ) as surf_pres_Pa,
     cast( windspeed_ms as {{dbt_utils.type_numeric()}} ) as windspeed_ms,
     cast( winddirection_deg as {{dbt_utils.type_numeric()}} ) as winddirection_deg,
-    cast( rho_kgm3 as {{dbt_utils.type_numeric()}} ) as rho_kgm3,
-    1 as interval_n,
-    'hour' as interval_units
+    cast( rho_kgm3 as {{dbt_utils.type_numeric()}} ) as rho_kgm3
 from {{ref('seed_la_haute_borne_merra2_sample')}}
---
+++ models/staging/entr_sample_data/map_entr_merra2_sample.sql
@@ -0,0 +1,11 @@
+with
+    src as (select * from {{ref('seed_la_haute_borne_merra2_tag_map')}})
+
+select
+    cast(entr_tag_name as {{dbt_utils.type_string()}}) as entr_tag_name,
+    cast(source_tag_name as {{dbt_utils.type_string()}}) as source_tag_name,
+    cast(interval_n as {{dbt_utils.type_numeric()}}) as interval_n,
+    cast(interval_units as {{dbt_utils.type_string()}}) as interval_units,
+    cast(value_type as {{dbt_utils.type_string()}}) as value_type,
+    cast(value_units as {{dbt_utils.type_string()}}) as value_units
+from src
