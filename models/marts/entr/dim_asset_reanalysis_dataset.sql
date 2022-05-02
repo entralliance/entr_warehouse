68e8dc8a55f3e1914285cea0fc9824a76b6222b5
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-05-02 17:25:40 -0400

    add missing parenthesis

--- models/marts/entr/dim_asset_reanalysis_dataset.sql
+++ models/marts/entr/dim_asset_reanalysis_dataset.sql
@@ -4,5 +4,5 @@
 
 select
     cast(reanalysis_dataset_id as {{dbt_utils.type_int()}}) as reanalysis_dataset_id,
-    cast(reanalysis_dataset_name as {{dbt_utils.type_string()}} as reanalysis_dataset_name
+    cast(reanalysis_dataset_name as {{dbt_utils.type_string()}}) as reanalysis_dataset_name
 from {{ref('seed_asset_reanalysis_dataset')}}
