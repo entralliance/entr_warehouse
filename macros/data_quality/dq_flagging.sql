e9196a4521333e58e4dac81ab8ae11cdb1f213ac
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-06-27 03:53:03 -0400

    build flagging & scrubbing macros

---
+++ macros/data_quality/dq_flagging.sql
@@ -0,0 +1,56 @@
+{% macro min_max_condition(column_name, min, max, where, inclusive=false) %}
+    {% if inclusive is false %}
+    {% set min_comparison = '<' %}
+    {% set max_comparison = '>' %}
+    {% else %}
+    {% set min_comparison = '<=' %}
+    {% set min_comparison = '>=' %}
+    {% endif %}
+    ({{ column_name }} {{min_comparison}} {{min}} or {{ column_name }} {{max_comparison}} {{max}}) {% if where is defined %} and {{where}} {% endif %}
+{% endmacro %}
+
+{% macro flag_min_max(column_name, min, max, alias, where='1=1', inclusive=false) %}
+    case
+        when {{min_max_condition(column_name, min, max, where, inclusive)}} then 1
+        else 0
+    end as {% if alias is defined %} {{alias}} {% else %} {{ 'flag_min_max_' + column_name}} {% endif %}
+{% endmacro %}
+
+{% macro scrub_min_max(column_name, min, max, alias, where='1=1', inclusive=false) %}
+    case
+        when {{min_max_condition(column_name, min, max, where, inclusive)}} then null
+        else {{column_name}}
+    end as {% if alias is defined %} {{alias}} {% else %} {{ column_name + '_min_max_scrubbed'}} {% endif %}
+{% endmacro %}
+
+{% macro stale_condition(column_name, datetime_column, partition_by_cols, exclude_values=[], lag_length=1) %}
+    {% if partition_by_columns is defined %} 
+        {{ column_name }} = lag( {{ column_name }}, {{ lag_length }} ) over ( partition by {{ partition_by_columns}} order by {{datetime_column}})
+    {%- else -%}
+        {{ column_name }} = lag( {{ column_name }}, {{ lag_length }} ) over ( order by {{datetime_column}})
+    {% endif %}
+    {% if exclude_values|length > 0 %} 
+        and {{ column_name }} not in ({{ jinja_list_to_sql(exclude_values) }})
+    {% endif %}
+{% endmacro %}
+
+{% macro flag_stale(column_name, datetime_column, partition_by_cols, exclude_values=[], lag_length=1) %}
+    case
+        when {{stale_condition(column_name, datetime_column, partition_by_cols, exclude_values, lag_length)}} then 1
+        else 0
+    end as {% if alias is defined %} {{alias}} {% else %} {{ 'flag_stale_' + column_name}} {% endif %}
+{% endmacro %}
+
+{% macro scrub_stale(column_name, datetime_column, partition_by_cols, alias, exclude_values=[], lag_length=1) %}
+    case
+        when {{stale_condition(column_name, datetime_column, partition_by_cols, exclude_values, lag_length)}} then null
+        else {{column_name}}
+    end as {% if alias is defined %} {{alias}} {% else %} {{ column_name + '_stale_scrubbed'}} {% endif %}
+{% endmacro %}
+
+{% macro scrub_min_max_stale(column_name, datetime_column, min, max, partition_by_cols, alias, exclude_values=[], lag_length=1, where='1=1', inclusive=false) %}
+    case
+        when ({{stale_condition(column_name, datetime_column, partition_by_cols, exclude_values, lag_length)}}) or ({{min_max_condition(column_name, min, max, where, inclusive)}}) then null
+        else {{column_name}}
+    end as {% if alias is defined %} {{alias}} {% else %} {{ column_name + '_stale_scrubbed'}} {% endif %}
+{% endmacro %}
