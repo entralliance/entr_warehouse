e8b221e44b7c5afe6e23ebdb96fe357f042d89f7
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-06-30 17:46:43 -0400

    add quote option

--- macros/utils/jinja_list_to_sql.sql
+++ macros/utils/jinja_list_to_sql.sql
@@ -1,9 +1,12 @@
 
 {# This macro converts a jinja list into lines for selection in a sql statement #}
-{% macro jinja_list_to_sql(jinja_list, exclude = [], relation_alias=False, field_separator=',\n') %}
+{% macro jinja_list_to_sql(jinja_list, exclude = [], relation_alias=False, field_separator=',\n', quote_identifiers=false) %}
     {%- for col in jinja_list %}
         {%- if col not in exclude %}
-            {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{col}}{%- if not loop.last %}{{ field_separator }}{% endif -%}
+            {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}
+                {%- if quote_identifiers %}{{adapter.quote(col)}}
+                {%- else %}{{col}}{% endif -%}
+            {%- if not loop.last %}{{ field_separator }}{% endif -%}
         {% endif -%}
     {%- endfor -%}    
 {% endmacro %}
