98d4135676119dfb252a57724c1f54c8a92b2974
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2022-06-30 17:45:39 -0400

    dq macro cleanup

--- macros/data_quality/dq_flagging.sql
+++ macros/data_quality/dq_flagging.sql
@@ -4,15 +4,15 @@
     {% set max_comparison = '>' %}
     {% else %}
     {% set min_comparison = '<=' %}
-    {% set min_comparison = '>=' %}
+    {% set max_comparison = '>=' %}
     {% endif %}
     ({{ column_name }} {{min_comparison}} {{min}} or {{ column_name }} {{max_comparison}} {{max}}) {% if where is defined %} and {{where}} {% endif %}
 {% endmacro %}
 
 {% macro flag_min_max(column_name, min, max, alias, where='1=1', inclusive=false) %}
     case
-        when {{min_max_condition(column_name, min, max, where, inclusive)}} then 1
-        else 0
+        when {{min_max_condition(column_name, min, max, where, inclusive)}} then true
+        else false
     end as {% if alias is defined %} {{alias}} {% else %} {{ 'flag_min_max_' + column_name}} {% endif %}
 {% endmacro %}
 
@@ -25,20 +25,20 @@
 
 {% macro stale_condition(column_name, datetime_column, partition_by_cols, exclude_values=[], lag_length=1) %}
     {% if partition_by_columns is defined %} 
-        {{ column_name }} = lag( {{ column_name }}, {{ lag_length }} ) over ( partition by {{ partition_by_columns}} order by {{datetime_column}})
+        {{ adapter.quote(column_name) }} = lag( {{ adapter.quote(column_name) }}, {{ lag_length }} ) over ( partition by {{ partition_by_columns}} order by {{adapter.quote(datetime_column)}})
     {%- else -%}
-        {{ column_name }} = lag( {{ column_name }}, {{ lag_length }} ) over ( order by {{datetime_column}})
+        {{ adapter.quote(column_name) }} = lag( {{ adapter.quote(column_name) }}, {{ lag_length }} ) over ( order by {{adapter.quote(datetime_column)}})
     {% endif %}
     {% if exclude_values|length > 0 %} 
-        and {{ column_name }} not in ({{ jinja_list_to_sql(exclude_values) }})
+        and {{ adapter.quote(column_name) }} not in ({{ jinja_list_to_sql(exclude_values) }})
     {% endif %}
 {% endmacro %}
 
 {% macro flag_stale(column_name, datetime_column, partition_by_cols, exclude_values=[], lag_length=1) %}
     case
-        when {{stale_condition(column_name, datetime_column, partition_by_cols, exclude_values, lag_length)}} then 1
-        else 0
-    end as {% if alias is defined %} {{alias}} {% else %} {{ 'flag_stale_' + column_name}} {% endif %}
+        when {{stale_condition(column_name, datetime_column, partition_by_cols, exclude_values, lag_length)}} then true
+        else false
+    end as {% if alias is defined %} {{alias}} {% else %} {{ adapter.quote('flag_stale_' + column_name) }} {% endif %}
 {% endmacro %}
 
 {% macro scrub_stale(column_name, datetime_column, partition_by_cols, alias, exclude_values=[], lag_length=1) %}
